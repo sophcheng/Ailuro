f19cd0012a24b10ec9c44b70ce5788528b061ae4
sophcheng <sophiecheng181@gmail.com>
2024-12-28 20:40:56 -0500

    Initial Commit

---
+++ Ailuro/AiluroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiluroApp.swift
+//  Ailuro
+//
+//  Created by Sophie Cheng on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AiluroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiluroTests/AiluroTests.swift
@@ -0,0 +1,17 @@
+//
+//  AiluroTests.swift
+//  AiluroTests
+//
+//  Created by Sophie Cheng on 12/28/24.
+//
+
+import Testing
+@testable import Ailuro
+
+struct AiluroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
